a66644db0bbcdc05856b7f660eb293e02ed14340
aem-0 <backendguy_2.10.25@proton.me>
2026-08-11 15:26:32 +0330

    feat: add agent loop and refactor read tool

--- app/main.py
+++ app/main.py
@@ -23,51 +23,63 @@ def main():
         raise RuntimeError("OPENROUTER_API_KEY is not set")
 
     client = OpenAI(api_key=API_KEY, base_url=BASE_URL)
-
-    chat = client.chat.completions.create(
-        # I chose this model for my tests.
-        # Take a look at OpenRouter for other models to replace it.
-        model="openrouter/free",
-        messages=[{"role": "user", "content": args.p}],
-        tools=[
-            {
-                "type": "function",
-                "function": {
-                    "name": "Read",
-                    "description": "Read and return the contents of a file",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "file_path": {
-                                "type": "string",
-                                "description": "The path to the file to read",
-                            }
+    messages = ([{"role": "user", "content": args.p}])
+    while True:
+        chat = client.chat.completions.create(
+            # I chose this model for my tests.
+            # Take a look at OpenRouter for other models to replace it.
+            model="openrouter/free",
+            messages=messages,
+            tools=[
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "Read",
+                        "description": "Read and return the contents of a file",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "file_path": {
+                                    "type": "string",
+                                    "description": "The path to the file to read",
+                                }
+                            },
+                            "required": ["file_path"],
                         },
-                        "required": ["file_path"],
                     },
-                },
-            }
-        ],
-    )
+                }
+            ],
+        )
 
-    if not chat.choices or len(chat.choices) == 0:
-        raise RuntimeError("no choices in response")
+        if not chat.choices or len(chat.choices) == 0:
+            raise RuntimeError("no choices in response")
 
-    message = chat.choices[0].message
-    print("Logs from your program will appear here!", file=sys.stderr)
+        message = chat.choices[0].message
+        messages.append(message.model_dump())
 
-    if message.tool_calls:
-        tool_call = message.tool_calls[0]
-        if tool_call.function.name == "Read":
-            args_dict = json.loads(tool_call.function.arguments)
-            file_path = args_dict.get("file_path")
+        if message.tool_calls:
+            for tool_call in message.tool_calls:
+                if tool_call.function.name == "Read":
+                    args_dict = json.loads(tool_call.function.arguments)
+                    file_path = args_dict.get("file_path")
+                    file_content = ""
+                    if file_path and Path(file_path).exists():
+                        file_content = Path(file_path).read_text()
+                    else:
+                        file_content = f"Error: File '{file_path}' not found."
 
-            if file_path:
-                content = Path(file_path).read_text()
-                print(content, end="")
-    else:
-        if message.content:
-            print(message.content)
+                    messages.append(
+                        {
+                            "role": "tool",
+                            "tool_call_id": tool_call.id,
+                            "content": file_content,
+                        }
+                    )
+        else:
+            print("Logs from your program will appear here!", file=sys.stderr)
+            if message.content:
+                print(message.content)
+            break
 
 
 if __name__ == "__main__":
